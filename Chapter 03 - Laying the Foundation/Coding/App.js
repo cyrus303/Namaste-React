7860f4f7bbc2f12525a60b5e69b838d31a6e6432
Sachin <sachinsunny303@gmail.com>
2023-08-10 07:23:01 +0100

    exploring jsx syntax

--- Chapter 03 - Laying the Foundation/Coding/App.js
+++ Chapter 03 - Laying the Foundation/Coding/App.js
@@ -28,7 +28,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', {}, 'NamasteElement');
+const heading = React.createElement(
+  'h1',
+  {id: 'heading'},
+  'NamasteElement'
+);
+
+const JsxHeading = <h1 id="heading">Hello Sachin</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(JsxHeading);
